ea83f2f8a59e9421527d0fd4d485921f6dbfffe6
Job Hernandez <hj93@protonmail.com>
2024-07-04 10:29:40 -0700

    define data

--- src/ToANormalForm.hs
+++ src/ToANormalForm.hs
@@ -7,3 +7,27 @@ isAtomic (Bool b) = True
 isAtomic (Int a) = True
 isAtomic (Var v) = True
 isAtomic _ = False
+
+data AtomicExp = AInt Int | ABool Bool | AVar String deriving Show
+
+data ComplexExp =
+  CIf AtomicExp AnfExp AnfExp
+  | CSet Exp AnfExp deriving Show
+  
+data AnfExp =
+  AExp AtomicExp
+  | CExp ComplexExp
+  | Let [(String, ComplexExp)] AnfExp deriving Show
+  
+toanf :: Exp -> AnfExp
+toanf (If (Bool x) thn els) =
+  CExp (CIf (ABool x) (toanf thn) (toanf els))
+
+toanf (If (Int x) thn els) =
+  CExp (CIf (AInt x) (toanf thn) (toanf els))
+
+toanf (If (Var x) thn els) =
+  CExp (CIf (AVar x) (toanf thn) (toanf els))
+  
+toanf (Int x) =
+  AExp (AInt x)
