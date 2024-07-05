c31bf9330826631d9a6526bd0233ceaaaa283e49
Job Hernandez <hj93@protonmail.com>
2024-07-05 09:49:01 -0700

    update

--- src/ToANormalForm.hs
+++ src/ToANormalForm.hs
@@ -36,9 +36,12 @@ toanf (If (If (Bool cnd) thn els) thn2 els2) counter =
 
 toanf (If (If cnd thn els) thn2 els2) counter =
   let tempName = "temp_" ++ show counter in
-  let tempName2 = "temp_" ++ show (counter + 1) in
-    CLet [(tempName, (tocomplex cnd))] (CLet [(tempName2, (CIf (AVar tempName) (toanf thn (counter + 2)) (toanf els (counter + 2))))] (toanf (If (Var tempName2) thn2 els2) (counter + 2)))
-
+    let tempName2 = "temp_" ++ show (counter + 1) in
+      let tempName3 = "temp_" ++ show (counter + 2) in
+        case cnd of
+          (If (Bool b) thn3 els3) -> CLet [(tempName, CIf (ABool b) (toanf thn3 (counter+2)) (toanf els3 (counter+2)))] (CLet [(tempName2, (CIf (AVar tempName) (toanf thn (counter+3)) (toanf els (counter+3))))] (toanf (If (Var tempName2) thn2 els2) (counter + 4)))
+          (If (Less a b) thn3 els3) -> CLet [(tempName, (tocomplex (Less a b)))] (CLet [(tempName2, (CIf (AVar tempName) (toanf thn3 (counter + 2)) (toanf els3 (counter+2))))]  (CLet [(tempName3, (CIf (AVar tempName2) (toanf thn (counter + 3)) (toanf els (counter + 3))))] (toanf (If (Var tempName3) thn2 els2) (counter + 4))))
+          
 toanf (Int x) counter =
   AExp (AInt x)
 
