42985668daa29102fe9fd1512b629359fe93f8a7
Job Hernandez <hj93@protonmail.com>
2024-07-06 08:10:34 -0700

    match a new exp

--- src/ToANormalForm.hs
+++ src/ToANormalForm.hs
@@ -13,24 +13,23 @@ data AtomicExp = AInt Int | ABool Bool | AVar String deriving Show
 data ComplexExp =
   CIf AtomicExp AnfExp AnfExp
   | CSet Exp AnfExp
-  | CLess AtomicExp AtomicExp deriving Show
-  
+  | CLess AtomicExp AtomicExp
+  | CPlus AtomicExp AtomicExp deriving Show
 data AnfExp =
   AExp AtomicExp
   | CExp ComplexExp
   | CLet [(String, AnfExp)] AnfExp deriving Show
 
+  
 toanf :: Exp -> Int -> AnfExp
 toanf (If (Bool x) thn els) counter =
   CExp (CIf (ABool x) (toanf thn counter) (toanf els counter))
 
 toanf (If (Var x) thn els) counter =
   CExp (CIf (AVar x) (toanf thn counter) (toanf els counter))
-  
 toanf (If (Less a b) thn els) counter =
   let tempName = "temp_" ++ show counter in
     CLet [(tempName, tocomplex (Less a b))] (toanf (If (Var tempName) thn els) (counter + 1))
-    
 toanf (If (If (Bool cnd) thn els) thn2 els2) counter =
   let tempName = "temp_" ++ show counter in
     CLet [(tempName, CExp (CIf (ABool cnd) (toanf thn counter) (toanf els counter)))] (toanf (If (Var tempName) thn2 els2) (counter + 1))
@@ -61,9 +60,19 @@ toanf (Let [(Var x, If (Bool b) thn els)] body) counter =
 toanf (Let [(Var x, If (Less a b) thn els)] body) counter =
   let tempName = "temp_" ++ show counter in
     CLet [(tempName, (tocomplex (Less a b)))] (CLet [(x, CExp (CIf (AVar tempName) (toanf thn (counter + 1)) (toanf els (counter + 1))))] (toanf body (counter + 1)))
-  
+
+toanf (Let [(Var x, (Let [(Var v, Int e)] body))] body2) counter =
+  CLet [(v, AExp (AInt e))] (CLet [(x, tocomplex body)] (toanf body2 counter))
+
+toanf (Plus (Var v) (Int x)) counter =
+  CExp (CPlus (AVar v) (AInt x))
 tocomplex :: Exp -> AnfExp
 tocomplex (Less (Int a) (Int b)) =
  CExp (CLess (AInt a) (AInt b))
 
+tocomplex (Plus (Int a) (Int b)) =
+  CExp (CPlus (AInt a) (AInt b))
+
+tocomplex (Plus (Var a) (Int b)) =
+  CExp (CPlus (AVar a) (AInt b))
 tocomplex _ = error "tocomplex: unsupported expression"
