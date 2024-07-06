5b68805121102a82d2ee014e02d4a465c349c37d
Job Hernandez <hj93@protonmail.com>
2024-07-05 20:55:43 -0700

    refactor data def

--- src/ToANormalForm.hs
+++ src/ToANormalForm.hs
@@ -18,7 +18,7 @@ data ComplexExp =
 data AnfExp =
   AExp AtomicExp
   | CExp ComplexExp
-  | CLet [(String, ComplexExp)] AnfExp deriving Show
+  | CLet [(String, AnfExp)] AnfExp deriving Show
 
 toanf :: Exp -> Int -> AnfExp
 toanf (If (Bool x) thn els) counter =
@@ -32,15 +32,15 @@ toanf (If (Var x) thn els) counter =
 
 toanf (If (If (Bool cnd) thn els) thn2 els2) counter =
   let tempName = "temp_" ++ show counter in
-    CLet [(tempName, CIf (ABool cnd) (toanf thn counter) (toanf els counter))] (toanf (If (Var tempName) thn2 els2) (counter + 1))
+    CLet [(tempName, CExp (CIf (ABool cnd) (toanf thn counter) (toanf els counter)))] (toanf (If (Var tempName) thn2 els2) (counter + 1))
 
 toanf (If (If cnd thn els) thn2 els2) counter =
   let tempName = "temp_" ++ show counter in
     let tempName2 = "temp_" ++ show (counter + 1) in
       let tempName3 = "temp_" ++ show (counter + 2) in
         case cnd of
-          (If (Bool b) thn3 els3) -> CLet [(tempName, CIf (ABool b) (toanf thn3 (counter+2)) (toanf els3 (counter+2)))] (CLet [(tempName2, (CIf (AVar tempName) (toanf thn (counter+3)) (toanf els (counter+3))))] (toanf (If (Var tempName2) thn2 els2) (counter + 4)))
-          (If (Less a b) thn3 els3) -> CLet [(tempName, (tocomplex (Less a b)))] (CLet [(tempName2, (CIf (AVar tempName) (toanf thn3 (counter + 2)) (toanf els3 (counter+2))))]  (CLet [(tempName3, (CIf (AVar tempName2) (toanf thn (counter + 3)) (toanf els (counter + 3))))] (toanf (If (Var tempName3) thn2 els2) (counter + 4))))
+          (If (Bool b) thn3 els3) -> CLet [(tempName, CExp (CIf (ABool b) (toanf thn3 (counter+2)) (toanf els3 (counter+2))))] (CLet [(tempName2, CExp (CIf (AVar tempName) (toanf thn (counter+3)) (toanf els (counter+3))))] (toanf (If (Var tempName2) thn2 els2) (counter + 4)))
+          (If (Less a b) thn3 els3) -> CLet [(tempName, (tocomplex (Less a b)))] (CLet [(tempName2, CExp (CIf (AVar tempName) (toanf thn3 (counter + 2)) (toanf els3 (counter+2))))]  (CLet [(tempName3, CExp (CIf (AVar tempName2) (toanf thn (counter + 3)) (toanf els (counter + 3))))] (toanf (If (Var tempName3) thn2 els2) (counter + 4))))
           
 toanf (Int x) counter =
   AExp (AInt x)
@@ -48,9 +48,9 @@ toanf (Int x) counter =
 toanf (Bool x) counter =
   AExp (ABool x)
   
-tocomplex :: Exp -> ComplexExp
+tocomplex :: Exp -> AnfExp
 
 tocomplex (Less (Int a) (Int b)) =
-  CLess (AInt a) (AInt b)
+ CExp (CLess (AInt a) (AInt b))
 
 tocomplex _ = error "tocomplex: unsupported expression"
