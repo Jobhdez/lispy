817060e79a2492027e41fed951f49948a946b5bc
Job Hernandez <hj93@protonmail.com>
2024-07-06 12:02:48 -0700

    refactor data def of CLet

--- src/ToANormalForm.hs
+++ src/ToANormalForm.hs
@@ -15,10 +15,11 @@ data ComplexExp =
   | CSet Exp AnfExp
   | CLess AtomicExp AtomicExp
   | CPlus AtomicExp AtomicExp deriving Show
+
 data AnfExp =
   AExp AtomicExp
   | CExp ComplexExp
-  | CLet [(String, AnfExp)] AnfExp deriving Show
+  | CLet [(AtomicExp, AnfExp)] AnfExp deriving Show
 
   
 toanf :: Exp -> Int -> AnfExp
@@ -28,19 +29,19 @@ toanf (If (Bool x) thn els) counter =
 toanf (If (Var x) thn els) counter =
   CExp (CIf (AVar x) (toanf thn counter) (toanf els counter))
 toanf (If (Less a b) thn els) counter =
-  let tempName = "temp_" ++ show counter in
-    CLet [(tempName, tocomplex (Less a b))] (toanf (If (Var tempName) thn els) (counter + 1))
+  let tempName = AVar ("temp_" ++ show counter) in
+    CLet [(tempName, tocomplex (Less a b))] (toanf (If (Var ("temp_" ++ show counter)) thn els) (counter + 1))
 toanf (If (If (Bool cnd) thn els) thn2 els2) counter =
-  let tempName = "temp_" ++ show counter in
-    CLet [(tempName, CExp (CIf (ABool cnd) (toanf thn counter) (toanf els counter)))] (toanf (If (Var tempName) thn2 els2) (counter + 1))
+  let tempName = AVar ("temp_" ++ show counter) in
+    CLet [(tempName, CExp (CIf (ABool cnd) (toanf thn counter) (toanf els counter)))] (toanf (If (Var ("temp_" ++ show counter)) thn2 els2) (counter + 1))
 
 toanf (If (If cnd thn els) thn2 els2) counter =
-  let tempName = "temp_" ++ show counter in
-    let tempName2 = "temp_" ++ show (counter + 1) in
-      let tempName3 = "temp_" ++ show (counter + 2) in
+  let tempName = AVar ("temp_" ++ show counter) in
+    let tempName2 = AVar ("temp_" ++ show (counter + 1)) in
+      let tempName3 = AVar ("temp_" ++ show (counter + 2)) in
         case cnd of
-          (If (Bool b) thn3 els3) -> CLet [(tempName, CExp (CIf (ABool b) (toanf thn3 (counter+2)) (toanf els3 (counter+2))))] (CLet [(tempName2, CExp (CIf (AVar tempName) (toanf thn (counter+3)) (toanf els (counter+3))))] (toanf (If (Var tempName2) thn2 els2) (counter + 4)))
-          (If (Less a b) thn3 els3) -> CLet [(tempName, (tocomplex (Less a b)))] (CLet [(tempName2, CExp (CIf (AVar tempName) (toanf thn3 (counter + 2)) (toanf els3 (counter+2))))]  (CLet [(tempName3, CExp (CIf (AVar tempName2) (toanf thn (counter + 3)) (toanf els (counter + 3))))] (toanf (If (Var tempName3) thn2 els2) (counter + 4))))
+          (If (Bool b) thn3 els3) -> CLet [(tempName, CExp (CIf (ABool b) (toanf thn3 (counter+2)) (toanf els3 (counter+2))))] (CLet [(tempName2, CExp (CIf (AVar ("temp_" ++ show counter)) (toanf thn (counter+3)) (toanf els (counter+3))))] (toanf (If (Var ("temp_" ++ show (counter + 1))) thn2 els2) (counter + 4)))
+          (If (Less a b) thn3 els3) -> CLet [(tempName, (tocomplex (Less a b)))] (CLet [(tempName2, CExp (CIf (AVar ("temp_" ++ show counter)) (toanf thn3 (counter + 2)) (toanf els3 (counter+2))))]  (CLet [(tempName3, CExp (CIf (AVar ("temp_" ++ show (counter + 1))) (toanf thn (counter + 3)) (toanf els (counter + 3))))] (toanf (If (Var ("temp_" ++ show (counter + 2))) thn2 els2) (counter + 4))))
    
 toanf (Int x) counter =
   AExp (AInt x)
@@ -49,20 +50,20 @@ toanf (Bool x) counter =
   AExp (ABool x)
 
 toanf (Let [(Var x, Int y)] (Int z)) counter =
-  (CLet [(x, AExp (AInt y))] (AExp (AInt z)))
+  (CLet [(AVar x, AExp (AInt y))] (AExp (AInt z)))
 
 toanf (Let [(Var x, Var y)] (Int z)) counter =
-  CLet [(x, AExp (AVar y))] (AExp (AInt z))
+  CLet [(AVar x, AExp (AVar y))] (AExp (AInt z))
 
 toanf (Let [(Var x, If (Bool b) thn els)] body) counter =
-  CLet [(x, CExp (CIf (ABool b) (toanf thn counter) (toanf els counter)))] (toanf body counter)
+  CLet [(AVar x, CExp (CIf (ABool b) (toanf thn counter) (toanf els counter)))] (toanf body counter)
 
 toanf (Let [(Var x, If (Less a b) thn els)] body) counter =
-  let tempName = "temp_" ++ show counter in
-    CLet [(tempName, (tocomplex (Less a b)))] (CLet [(x, CExp (CIf (AVar tempName) (toanf thn (counter + 1)) (toanf els (counter + 1))))] (toanf body (counter + 1)))
+  let tempName = AVar ("temp_" ++ show counter) in
+    CLet [(tempName, (tocomplex (Less a b)))] (CLet [(AVar x, CExp (CIf (AVar ("temp_" ++ show counter)) (toanf thn (counter + 1)) (toanf els (counter + 1))))] (toanf body (counter + 1)))
 
 toanf (Let [(Var x, (Let [(Var v, Int e)] body))] body2) counter =
-  CLet [(v, AExp (AInt e))] (CLet [(x, tocomplex body)] (toanf body2 counter))
+  CLet [(AVar v, AExp (AInt e))] (CLet [(AVar x, tocomplex body)] (toanf body2 counter))
 
 toanf (Plus (Var v) (Int x)) counter =
   CExp (CPlus (AVar v) (AInt x))
@@ -71,7 +72,7 @@ toanf (Plus (Var v) (Var v2)) counter =
   CExp (CPlus (AVar v) (AVar v2))
 
 toanf (Let [(Var x, Int y)] (Let [(Var v, Int z)] body)) counter =
-  CLet [(x, AExp (AInt y))] (CLet [(v, AExp (AInt z))] (toanf body counter))
+  CLet [(AVar x, AExp (AInt y))] (CLet [(AVar v, AExp (AInt z))] (toanf body counter))
   
 tocomplex :: Exp -> AnfExp
 tocomplex (Less (Int a) (Int b)) =
