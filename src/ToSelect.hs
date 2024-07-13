09fbd76c8e7091096600f2e89c99c196541b6916
Job Hernandez <hj93@protonmail.com>
2024-07-13 12:11:11 -0700

    define the select instruction intermediate language

---
+++ src/ToSelect.hs
@@ -0,0 +1,26 @@
+module ToSelect where
+
+import Parser
+import ToAnf
+import ToCir
+
+data Immediate = Imm Int
+data Register = Reg String
+data MemoryRef = Ref String
+
+data Argument =
+  ArgImm Immediate
+  | ArgReg Register
+  | ArgMem MemoryRef
+
+data Instruction =
+  Addq Argument Argument
+  | Subq Argument Argument
+  | Negq Argument
+  | Movq Argument Argument
+  | Pushq Argument
+  | Popq Argument
+  | Jmp String
+  | Callq String Int
+  | Retq String
+  | Cmpq Argument Argument
