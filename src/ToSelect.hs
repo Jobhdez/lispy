1723e11babe515f0e9004b3ed881d178b71057bf
Job Hernandez <hj93@protonmail.com>
2024-07-13 19:56:54 -0700

    handle another use case

--- src/ToSelect.hs
+++ src/ToSelect.hs
@@ -38,6 +38,12 @@ toselect ((x:xs), blocks) =
       Addq (MemoryRef var) (MemoryRef var') : toselect (xs, blocks)
     Assign (CVar var) (CLess (CInt a) (CInt b)) ->
       Cmpq (Immediate b) (Immediate a) : Setg (Register "%al") : Movzbl (Register "%al") (MemoryRef var) : toselect (xs, blocks)
+    CLess (CVar var) (CInt n) ->
+      Cmpq (Immediate n) (MemoryRef var) : toselect (xs, blocks)
+    Assign (CVar var) (CPlus (CVar var') (CVar var'')) ->
+      Movq (MemoryRef var') (MemoryRef var) : Addq (MemoryRef var'') (MemoryRef var) : toselect (xs, blocks)
+    Assign (CVar var) (CPlus (CVar var') (CInt n)) ->
+      Movq (MemoryRef var') (MemoryRef var) : Addq (Immediate n) (MemoryRef var) : toselect (xs, blocks)
     IfGoto (CVar var) (Goto block) (Goto block') ->
       let blk = Map.lookup block blocks
           blk' = Map.lookup block' blocks
@@ -46,4 +52,11 @@ toselect ((x:xs), blocks) =
             Cmpq (Immediate 1) (MemoryRef var) : Jmp block : Label block : toselect (blkInstrs, blocks) ++ [Jmp block'] ++ Label block' : toselect (blk'Instrs, blocks) ++ toselect (xs, blocks)
           _ -> error "Block not found"
     CInt n -> Movq (Immediate n) (Register "%rax") : toselect (xs, blocks)
+    CBegin exps -> concatMap (\cirexp -> toselect ([cirexp], blocks)) exps ++ toselect (xs, blocks)
+    IfGotoLoop cnd (Goto block) ->
+      let blk = Map.lookup block blocks
+      in case blk of
+           (Just blk') ->
+             Label (block ++ ":") : toselect (blk', blocks) ++ toselect ([cnd], blocks) ++ [Jmp block] ++ toselect (xs, blocks)
+           _ -> error "Block not found"
     _ -> error "Unsupported CIR instruction"
